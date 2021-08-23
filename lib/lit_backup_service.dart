9ad9c712355d57bbb6187613a11ff8bef65eb9d5
eightgran <eightgran@gmail.com>
2021-08-23 21:13:17 +0200

    Export sub libraries

--- lib/lit_backup_service.dart
+++ lib/lit_backup_service.dart
@@ -1,7 +1,20 @@
+// Copyright 2021 LitLifeSoftware. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/// A Flutter package to create and restore backups, whose content is formatted
+/// as a JSON files.
+///
+/// To use, import `package:lit_backup_service/lit_backup_service.dart`.
+///
+/// Dependencies used:
+///
+/// * `path_provider`
+///
+/// To use the above mentioned dependencies, please include them
+/// separatly on the `pubspec.yaml` of your project. These will not be
+/// exported to avoid namespace issues.
 library lit_backup_service;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'model/models.dart';
+export 'controller/controllers.dart';
