17470e2632907e2114769c55cb198dd6998311b0
eightgran <eightgran@gmail.com>
2021-08-24 14:41:19 +0200

    Add default `Android` Media locations

---
+++ lib/data/android_media_locations.dart
@@ -0,0 +1,11 @@
+import 'package:lit_backup_service/model/models.dart';
+
+/// A [MediaLocation] pointing the the `Documents` Media folder on `Android`
+/// devices.
+const MediaLocation DOCUMENTS_LOCATION_ANDROID =
+    MediaLocation(path: "/storage/emulated/0/Documents/");
+
+/// A [MediaLocation] pointing the the `Download` Media folder on `Android`
+/// devices.
+const MediaLocation DOWNLOAD_LOCATION_ANDROID =
+    MediaLocation(path: "/storage/emulated/0/Download/");
